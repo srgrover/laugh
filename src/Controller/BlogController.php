b683eae3de1216f54386e441e56632e5ca25244a
srgrover <djgrovezone@gmail.com>
2019-04-03 18:03:45 +0200

    localizeddate y ahora se pueden eliminar comentarios

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,8 +1,12 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
@@ -19,7 +23,25 @@ class BlogController extends AbstractController
     }
 
 
-
+    /**
+     * @Route("/comment/delete/{id}", name="confirm_delete_comment", methods={"GET"})
+     * @param Comment $id
+     * @return Response
+     */
+    public function borrarDeVerdadAction(Comment $id)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        try {
+            $em->remove($id);
+            $em->flush();
+            $this->addFlash('estado', 'Comentario eliminado');
+        }
+        catch(ORMException $e) {
+            $this->addFlash('error', 'Ups! Algo salió mal. No se han podido eliminar');
+        }
+        return $this->redirectToRoute('view_post', array('id' => $id->getPost()->getId()));
+    }
 
 
 }
